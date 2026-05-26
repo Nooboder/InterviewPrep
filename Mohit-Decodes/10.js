027675a97f634f0aea0a6deba019f642e8208559
saptasingha7 <120020639+saptasingha7@users.noreply.github.com>
2026-05-27 01:09:50 +0530

    new

--- Mohit-Decodes/10.js
+++ Mohit-Decodes/10.js
@@ -11,3 +11,17 @@ for (let n of arr) {
 }
 
 console.log(unique);
+
+// const arr = [1, 2, 3, 4, 2, 1, 1]
+
+// // Find unique element of an array
+
+// console.log([...new Set(arr)])
+
+// //Find Duplicate element of an array
+
+// const duplicate = arr.filter((item, index) =>
+//     arr.indexOf(item) != index
+// )
+
+// console.log([...new Set(duplicate)])
