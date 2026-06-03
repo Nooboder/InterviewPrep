e6ba9a804ad8a614747e377d5cdc2d229a2ad507
sapta007 <saptasingha31@gmail.com>
2026-06-03 23:59:12 +0530

    changes

--- Code-Compile/2.js
+++ Code-Compile/2.js
@@ -80,3 +80,50 @@ obj.normalGreet();
 
 // Arrow: Hello, Jhon
 // Normal: Hello, sapta
+
+
+// 2.iii) Arrow Function and Normal Function 
+
+const obj1 = {
+    a: 1,
+    b: 2,
+    sum() {
+        return this.a + this.b;
+    }
+}
+
+
+const res = obj1.sum();
+console.log(res());
+
+// explain : In the above code, we have an object `obj1` with properties `a` and `b`, and a method `sum` that returns the sum of `a` and `b`. When we call `obj1.sum()`, it correctly returns the sum of `a` and `b`, which is 3. However, when we try to call `res()` after storing the result of `obj1.sum()` in the variable `res`, it will throw an error because `res` is not a function, but rather the result of the sum, which is a number (3).
+
+// To fix this, we should simply log the result of `obj1.sum()` directly without trying to call it as a function:
+// const res = obj1.sum();
+// console.log(res);
+// This will correctly output 3 without any errors.
+
+// other solution :
+const obj1 = {
+    a: 1,
+    b: 2,
+    sum() {
+        return this.a + this.b;
+    }
+}
+
+const res = obj1.sum;
+console.log(res()); // This will work because res is now a reference to the sum function, and when we call res(), it will execute the sum function with the correct context of obj1, allowing it to access this.a and this.b correctly.
+
+// bind method can also be used to fix the context of `this` when calling the function:
+const obj1 = {
+    a: 1,
+    b: 2,
+    sum() {
+        return this.a + this.b;
+    }
+}
+
+const res = obj1.sum.bind(obj1);
+console.log(res()); // This will also work because bind creates a new function with the context of obj1, allowing it to access this.a and this.b correctly when called.
+
