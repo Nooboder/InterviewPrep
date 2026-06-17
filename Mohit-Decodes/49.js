f378a61f949c920417339966b5328dadc9e60d12
saptasingha7 <saptasingha31@gmail.com>
2026-06-17 11:22:02 +0530

    new

--- Mohit-Decodes/49.js
+++ Mohit-Decodes/49.js
@@ -11,3 +11,32 @@ function reverseString(str) {
 const inputString = "Hello, World!";
 const reversedString = reverseString(inputString);
 console.log(reversedString); // Output: !dlroW ,olleH
+
+
+// reversed an array without using built in functions
+
+
+function reverseArray(arr) {
+  const reversed = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i]);
+  }
+
+  return reversed;
+}
+
+const arr = [1, 2, 3, 4, 5];
+console.log(reverseArray(arr)); // [5, 4, 3, 2, 1]
+
+
+
+
+
+// 3. Reverse Without Modifying Original Array
+const arr = [1, 2, 3, 4, 5];
+
+const reversed = [...arr].reverse();
+
+console.log(reversed); // [5, 4, 3, 2, 1]
+console.log(arr);      // [1, 2, 3, 4, 5]
