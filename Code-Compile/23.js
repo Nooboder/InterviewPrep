61ee3d092765c38cda7da18177f92f336c4449fb
saptasingha7 <saptasingha31@gmail.com>
2026-07-28 11:12:01 +0530

    new

--- Code-Compile/23.js
+++ Code-Compile/23.js
@@ -14,3 +14,21 @@ console.log(y);
 // x*2 = 11*2 ;
 
 // Output will be 22
+
+
+// Trick Question 4
+
+
+const arr = [1, 2, 3, 4, 5]
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        arr.splice(i, 1)
+    }
+}
+
+console.log(arr)
+
+// Output will be [1,3,5] because when we remove an element from the array using splice, the length of the array decreases and the index shifts. So, we skip the next element after removing an even number.
+
+
